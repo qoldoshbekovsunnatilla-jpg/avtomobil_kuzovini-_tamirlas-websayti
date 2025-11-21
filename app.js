6b7ef30c17f6bacedb1a62fd4988aa6ba71f6d0f
SUNNATILLA <qoldoshbekovsunnat@gmail.com>
2025-11-21 12:55:51 +0800

    tugamadi

---
+++ app.js
@@ -0,0 +1,14 @@
+let darkmode = document.querySelector('#mySwitch')
+darkmode.addEventListener('change', ozgar)
+
+function ozgar() {
+    if (!darkmode.checked) {
+        let body = document.querySelector('body')
+        body.style.backgroundColor = 'white'
+        body.style.color = 'black'
+    } else {
+        let body = document.querySelector('body')
+        body.style.backgroundColor = 'black'
+        body.style.color = 'white'
+    }
+}
